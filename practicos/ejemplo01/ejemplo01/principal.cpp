275e1f2b2406a2224d80783d2dc8946c8104d7c4
OmarChocamani <74500535+OmarChocamani@users.noreply.github.com>
2020-11-16 07:58:58 -0400

    Add files via upload

---
+++ practicos/ejemplo01/ejemplo01/principal.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Nombre: Omar Chocamani Llanque" << endl;
+	cout << "Carrera: Ingenieria de Sistemas" << endl;
+	cout << "Carnet Universitario: 35-4576" << endl;
+	return 0;
+}
